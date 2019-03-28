151355e5c33639c91c6bf05b3c2175b6d90c74db
yanhvo <34598655+yanhvo@users.noreply.github.com>
2019-03-28 13:41:25 +0300

    Add files via upload

--- Implementation/Common/Name.cs
+++ Implementation/Common/Name.cs
@@ -3,7 +3,7 @@
     /// <summary>
     ///     Скрытая реализация представления об имени человека.
     /// </summary>
-    internal struct Name
+    internal struct Name : IName
     {
         /*
          * TODO #1: Реализуйте интерфейс IName для структуры Name
@@ -18,11 +18,33 @@
         ///     Фамилия.
         /// </summary>
         public string Surname { get; }
-        
+
         /// <summary>
         ///     Отчество.
         /// </summary>
         public string Patronymic { get; }
-        
+
+        public string FullName { get; }
+
+        public string ShortName { get; }
+
+        public Name(string surname, string firstname, string patronymic)
+        {
+            FirstName = firstname;
+            Surname = surname;
+            Patronymic = patronymic;
+            FullName = surname + ' ' + firstname + ' ' + patronymic;
+            ShortName = surname + ' ' + firstname[0] + ". " + patronymic[0] + '.';
+        }
+
+        public Name(IName name)
+        {
+            FullName = name.FullName;
+            ShortName = name.ShortName;
+            var arr = FullName.Split(' ');
+            Surname = arr[0];
+            FirstName = arr[1];
+            Patronymic = arr[2];
+        }
     }
 }
